2247f3a23451b48381e69316880d51d7d947ab32
Vladis <vladismarkin@gmail.com>
2020-12-22 10:59:14 +0200

    Adding some comments and updating the lat state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,19 +3,29 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
-    super(props); // A ref to React.Component constructor function. Allways the 1st thing.\
+    super(props); // A ref to React.Component constructor function. Allways the 1st thing.
 
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
     this.state = { lat: null };
   }
 
   // React says that we have to define render function.
   render() {
+    // We dont want to initialize things in render function,
+    // Because it is activated in every update of the state.
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // We called setState method evry time we want to update our state.
+        this.setState({ lat: position.coords.latitude });
+
+        // We did not:
+        // this.state.lat = position.coords.latitude;
+      },
       (err) => console.log(err)
     );
 
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
