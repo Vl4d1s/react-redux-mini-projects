271fca80967686ecb7bb68146ef5acf25f9b75e2
Vladis <vladismarkin@gmail.com>
2020-12-23 14:27:24 +0200

    Adding loader component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 import "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
@@ -43,7 +44,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+    return <Loader message={"Please accept location request."} />;
   }
 }
 
