ca86751bfaca4156fdaf8638273b9024819dd56c
Vladis <vladismarkin@gmail.com>
2020-12-24 14:01:22 +0200

    Adding onChange event

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // event.target.value holds user input
+  onInputChange(event) {}
+
   render() {
     return (
+      /* We do not put set of Parenthesis like: this.onInputChange()
+         Becuse then the function will activated when the component will
+         rendered. */
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
