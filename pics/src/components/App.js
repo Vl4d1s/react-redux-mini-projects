f520bb86e0d5a89850f6b96e44378f354d50a134
Vladis <vladismarkin@gmail.com>
2020-12-27 00:03:36 +0200

    Using axios for our AJAX request to https://unsplash.com/ API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID mtZRrpLPeXIfVOnFDYAHyKPyIOSufldIUImWcV4ng5I",
+      },
+    });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
+        {/* Moving onSearchSubmit callback to SearchBar component*/}
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
