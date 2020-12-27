d01af43ff02120e345c1ef1c3e8c13c7e0253d34
Vladis <vladismarkin@gmail.com>
2020-12-27 10:10:24 +0200

    Adding ImageCard component to render images one by one

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map((image) => {
     // We have to assign key element to rendered list in react,
     // And we can assign it only to the root div.
-    return <img alt={description} key={id} src={urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
