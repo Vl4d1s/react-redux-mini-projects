deee82d599dba5550a7eec83a4da275c7b73b83b
Vladis <vladismarkin@gmail.com>
2021-01-04 15:59:44 +0200

    Adding VidepDetail component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
-const KEY = 'AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE';
+const KEY = "AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
-        part: 'snippet',
+        part: "snippet",
         maxResults: 5,
-        type: 'video',
+        type: "video",
         key: KEY,
       },
     });
@@ -22,11 +23,19 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
