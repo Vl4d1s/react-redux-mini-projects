cbd724bde0e63ccfdfbdb5d6c16ca27b66cbb9a1
Vladis <vladismarkin@gmail.com>
2021-01-08 00:43:24 +0200

    Moving item props from the app component to the accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "Who is Vladis?",
+    content: "Vladis is forth year software engineer student.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers.",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use react by creating components",
+  },
+];
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
