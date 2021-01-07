7a50ff0974737c0a9096c8f7a5b4ec26fef8f68b
Vladis <vladismarkin@gmail.com>
2021-01-08 01:01:10 +0200

    Rendering the accordion component content

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  return <div>{items.length}</div>;
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 };
 
 export default Accordion;
