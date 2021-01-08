7e76bdff3af72b294b573b4a8548fda27544a5c2
Vladis <vladismarkin@gmail.com>
2021-01-08 06:28:41 +0200

    Adding useEffect

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+//import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 
-const items = [
-  {
-    title: "Who is Vladis?",
-    content: "Vladis is forth year software engineer student.",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers.",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use react by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "Who is Vladis?",
+//     content: "Vladis is forth year software engineer student.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use react by creating components",
+//   },
+// ];
 
 const App = () => {
   return (
