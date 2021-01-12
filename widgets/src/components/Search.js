cabf974653adfb6106cb0f4678c5f8f10268a4af
Vladis <vladismarkin@gmail.com>
2021-01-12 19:44:18 +0200

    Rendering the results

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,9 +1,42 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programing");
+  const [results, setResults] = useState([]);
+  // [term] means: Run after every render
+  // if data has changed since last render.
+  console.log(results);
 
-  useEffect(() => {}, [term]);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    // When the component rendered on the fist time the value of
+    // term = "" and we don't want to search empty string
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -18,6 +51,9 @@ const Search = () => {
             className="input"
           />
         </div>
+      </div>
+      <div className="ui celled list" key={renderedResults.pageid}>
+        {renderedResults}
       </div>
     </div>
   );
