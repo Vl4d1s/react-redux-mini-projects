2dd38f9f74ab0a90567741738fd75f416525fadc
Vladis <vladismarkin@gmail.com>
2021-01-16 12:51:45 +0200

    Making use of useRef

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    // First all the addEventListener get calls and then React EventListeners.
+    // so this is the first addEventListener that called.
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        if (ref.current && ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      // for react v17
+      { capture: true }
+    );
+  }, []);
+
   const renderdOptions = options.map((option) => {
     if (option.value === selected.value) {
       // If user selected some opting don't show it twice.
@@ -11,6 +30,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         key={option.value}
         className="item"
         onClick={() => {
+          // the Second addEventListener that will called.
           onSelectedChange(option);
         }}
       >
@@ -20,13 +40,21 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            // the thired addEventListener that will called.
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderdOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderdOptions}
+          </div>
         </div>
       </div>
     </div>
