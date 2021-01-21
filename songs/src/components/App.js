e9e3c37ec49e53509efe6c69694035032c391f6e
Vladis <vladismarkin@gmail.com>
2021-01-21 23:56:54 +0200

    Building List with Redux Data

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import SongList from "./SongList";
 
-export default () => {
+const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
+
+export default App;
