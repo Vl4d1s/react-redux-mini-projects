3624ec8b050abf23fe0a01419760201244ac931e
Vladis <vladismarkin@gmail.com>
2021-01-21 23:30:25 +0200

    Wiring up the provider

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 const songsReducer = () => {
   return [
     { title: "No Scrubs", duration: "4:05" },
@@ -11,3 +12,8 @@ const SelectedSongReducer = (selectedSong = null, action) => {
   if (action.type === "SONG_SELECTED") return action.payload;
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: SelectedSongReducer,
+});
