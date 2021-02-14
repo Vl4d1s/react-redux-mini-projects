b1aa027e1b951cbbc3f00a463deed4f3ed52a053
Vladis <vladismarkin@gmail.com>
2021-02-14 02:08:57 +0200

    fetchin only one user from mapStateToProps function

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -7,14 +7,16 @@ class UserHeader extends Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const user = this.props.user;
     if (!user) return <div>Loading...</div>;
     return <div>{user.name}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  // mapStateToProps gets a secound argument as well. a reference to the props
+  // of the main component
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
